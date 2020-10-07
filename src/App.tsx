04f0e2438d52685265f37841e6b84b2b4e439a5a
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-07 13:33:32 -0300

    Components names given

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { Main, Section, Sticky } from './styles'
+import GlobalStyle from './styles/global'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <GlobalStyle />
+
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
+          <Sticky className="first" />
+          <Sticky className="second" />
+        </Section>
+
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth"/>
+        </Section>
+      </Main>
+    </>
+  )
 }
 
-export default App;
+export default App
